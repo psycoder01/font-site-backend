e78d7e431f4f2a49555f4700aa433a9c7cc4bc7a
Aakash Chaudhary <aakash.chaudhary@wiseyak.com>
2024-08-24 20:08:55 +0545

    fix: migrated to postgres

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,11 +3,17 @@ import { Sequelize } from 'sequelize';
 
 dotenv.config();
 
-const { MYSQL_USER = '', MYSQL_PASSWORD = '', MYSQL_DB = '', MYSQL_HOST = '' } = process.env;
+const { DB_USER = '', DB_PASSWORD = '', DB_NAME = '', DB_HOST = '' } = process.env;
 
-export const sequelize = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PASSWORD, {
-    host: MYSQL_HOST,
-    dialect: 'mysql',
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 sequelize.sync({
